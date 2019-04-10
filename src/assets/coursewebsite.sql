ebfb76c6a97847adbb4c8d2817c57cd630fb3040
Josh Fagen <joshmfagen@gmail.com>
2019-04-10 13:27:03 -0400

    Updated structure and data in Database

--- src/assets/coursewebsite.sql
+++ src/assets/coursewebsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:81
--- Generation Time: Apr 10, 2019 at 12:27 AM
+-- Generation Time: Apr 10, 2019 at 07:21 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -27,8 +27,16 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `announcements`
 --
+-- Error reading structure for table coursewebsite.announcements: #1932 - Table 'coursewebsite.announcements' doesn't exist in engine
+-- Error reading data for table coursewebsite.announcements: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `coursewebsite`.`announcements`' at line 1
 
-CREATE TABLE `announcements` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements_table`
+--
+
+CREATE TABLE `announcements_table` (
   `id` int(11) NOT NULL,
   `teacherID` int(11) NOT NULL,
   `date` date NOT NULL,
@@ -37,83 +45,66 @@ CREATE TABLE `announcements` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `announcements`
+-- Dumping data for table `announcements_table`
 --
 
-INSERT INTO `announcements` (`id`, `teacherID`, `date`, `title`, `body`) VALUES
-(1, 2, '2018-01-10', 'Semester Starts Today', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi laoreet ex congue luctus semper. Aenean mattis erat sem, id sodales felis iaculis lobortis. In orci leo, bibendum non est nec, auctor dapibus lectus. Quisque porttitor lorem quam, sed dignissim magna ornare in. Nam accumsan velit a libero vehicula, et tempus mauris sodales. Donec ac ultricies enim. Duis non ante imperdiet, interdum tellus a, placerat enim. In scelerisque tortor at nunc placerat, at pharetra nisl luctus.'),
-(2, 3, '2019-03-08', 'Classes Cancelled Due to Inclement Weather', 'Sed ornare mauris sit amet sapien placerat, sagittis finibus felis suscipit. Duis interdum venenatis erat ac lobortis. Fusce elementum viverra mi sit amet placerat. Fusce malesuada purus vel dui sollicitudin, in luctus velit tincidunt. In ut ex nisl.'),
+INSERT INTO `announcements_table` (`id`, `teacherID`, `date`, `title`, `body`) VALUES
+(1, 2, '2019-01-10', 'Semester Starts Today', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi laoreet ex congue luctus semper. Aenean mattis erat sem, id sodales felis iaculis lobortis. In orci leo, bibendum non est nec, auctor dapibus lectus. Quisque porttitor lorem quam, sed dignissim magna ornare in. Nam accumsan velit a libero vehicula, et tempus mauris sodales. Donec ac ultricies enim. Duis non ante imperdiet, interdum tellus a, placerat enim. In scelerisque tortor at nunc placerat, at pharetra nisl luctus.'),
+(2, 1, '2019-02-08', 'Classes Cancelled Due to Inclement Weather', 'Sed ornare mauris sit amet sapien placerat, sagittis finibus felis suscipit. Duis interdum venenatis erat ac lobortis. Fusce elementum viverra mi sit amet placerat. Fusce malesuada purus vel dui sollicitudin, in luctus velit tincidunt. In ut ex nisl.'),
 (3, 1, '2019-03-27', 'Website Down Due to Routine Maintenance', 'Etiam ac dapibus nulla. Maecenas vel risus quis purus accumsan pretium. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id mi nec est euismod vestibulum. Fusce ut iaculis libero. Vivamus sed auctor ligula, nec ultrices dui. Sed quis condimentum lacus.'),
-(4, 4, '2019-04-07', 'Exams Starting This Week', 'Duis leo orci, pellentesque nec elementum ac, placerat eget tortor. Sed sapien felis, porta et sagittis non, convallis quis sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique quis mauris ac consectetur. Maecenas pretium nibh dolor, in mollis nisi blandit vel.'),
-(5, 2, '2019-04-10', 'Update to Anti-Plagirism Policy', 'Proin non enim a ipsum iaculis tempus. Vivamus volutpat, sem sit amet elementum imperdiet, nisi sapien dapibus lectus, at tristique erat sem in enim. Maecenas ornare risus ac urna consequat mattis. Nam quis ligula lacinia, tristique sem at, tristique metus. Nulla faucibus ex id suscipit auctor. Aliquam ut dictum lorem. Sed tincidunt viverra enim, at facilisis tellus rutrum vel.');
+(4, 2, '2019-04-07', 'Exams Starting This Week', 'Duis leo orci, pellentesque nec elementum ac, placerat eget tortor. Sed sapien felis, porta et sagittis non, convallis quis sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique quis mauris ac consectetur. Maecenas pretium nibh dolor, in mollis nisi blandit vel.'),
+(5, 1, '2019-04-10', 'Update to Anti-Plagirism Policy', 'Proin non enim a ipsum iaculis tempus. Vivamus volutpat, sem sit amet elementum imperdiet, nisi sapien dapibus lectus, at tristique erat sem in enim. Maecenas ornare risus ac urna consequat mattis. Nam quis ligula lacinia, tristique sem at, tristique metus. Nulla faucibus ex id suscipit auctor. Aliquam ut dictum lorem. Sed tincidunt viverra enim, at facilisis tellus rutrum vel.');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `assignments`
+-- Table structure for table `assignments_table`
 --
 
-CREATE TABLE `assignments` (
+CREATE TABLE `assignments_table` (
   `id` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `teachercopy` tinyint(1) NOT NULL,
-  `class` varchar(8) NOT NULL,
-  `releaseDate` date NOT NULL,
-  `dueDate` date NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `body` text NOT NULL,
-  `coursePercentage` double NOT NULL,
-  `studentGrade` double NOT NULL
+  `subject` varchar(64) NOT NULL,
+  `information` text NOT NULL,
+  `release_date` date NOT NULL,
+  `due_date` date NOT NULL,
+  `grade_weight` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `assignments`
--- Note: If studentGrade == -1, assignment is unsubmitted. If studentGrade == -2, assignment is submitted but not yet graded.
+-- Dumping data for table `assignments_table`
+--
 
-INSERT INTO `assignments` (`id`, `userid`, `teachercopy`, `class`, `releaseDate`, `dueDate`, `title`, `body`, `coursePercentage`, `studentGrade`) VALUES
-(1, 1, 0, 'COMP201', '2019-01-01', '2019-02-01', 'Assignment1', 'This is the first Assignment', 20, 59),
-(1, 1, 1, 'COMP201', '2019-01-01', '2019-02-01', 'Assignment1', 'This is the first Assignment', 20, 83),
-(1, 2, 0, 'COMP201', '2019-01-01', '2019-02-01', 'Assignment1', 'This is the first Assignment', 20, 90),
-(1, 3, 0, 'COMP201', '2019-01-01', '2019-02-01', 'Assignment1', 'This is the first Assignment', 20, 75),
-(1, 4, 0, 'COMP201', '2019-01-01', '2019-02-01', 'Assignment1', 'This is the first Assignment', 20, 72),
-(2, 1, 0, 'COMP201', '2019-03-25', '2019-04-15', 'Build A Fully-Functioning Website', 'Aliquam feugiat nisl ac orci mattis, in viverra arcu auctor. Aenean ac velit posuere, dictum est tristique, condimentum dui. Ut vitae magna eget est ornare tempus quis at velit. Nunc fringilla diam risus, sit amet scelerisque magna placerat hendrerit.', 30, -2),
-(2, 1, 1, 'COMP201', '2019-03-25', '2019-04-15', 'Build A Fully-Functioning Website', 'Aliquam feugiat nisl ac orci mattis, in viverra arcu auctor. Aenean ac velit posuere, dictum est tristique, condimentum dui. Ut vitae magna eget est ornare tempus quis at velit. Nunc fringilla diam risus, sit amet scelerisque magna placerat hendrerit.', 30, -1),
-(2, 2, 0, 'COMP201', '2019-03-25', '2019-04-15', 'Build A Fully-Functioning Website', 'Aliquam feugiat nisl ac orci mattis, in viverra arcu auctor. Aenean ac velit posuere, dictum est tristique, condimentum dui. Ut vitae magna eget est ornare tempus quis at velit. Nunc fringilla diam risus, sit amet scelerisque magna placerat hendrerit.', 30, -1),
-(2, 3, 0, 'COMP201', '2019-03-25', '2019-04-15', 'Build A Fully-Functioning Website', 'Aliquam feugiat nisl ac orci mattis, in viverra arcu auctor. Aenean ac velit posuere, dictum est tristique, condimentum dui. Ut vitae magna eget est ornare tempus quis at velit. Nunc fringilla diam risus, sit amet scelerisque magna placerat hendrerit.', 30, -1),
-(2, 4, 0, 'COMP201', '2019-03-25', '2019-04-15', 'Build A Fully-Functioning Website', 'Aliquam feugiat nisl ac orci mattis, in viverra arcu auctor. Aenean ac velit posuere, dictum est tristique, condimentum dui. Ut vitae magna eget est ornare tempus quis at velit. Nunc fringilla diam risus, sit amet scelerisque magna placerat hendrerit.', 30, -1),
-(3, 1, 0, 'DATA301', '2019-03-01', '2019-04-01', 'Database Questionnaire', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 15, 65),
-(3, 2, 0, 'DATA301', '2019-03-01', '2019-04-01', 'Database Questionnaire', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 15, 75),
-(3, 2, 1, 'DATA301', '2019-03-01', '2019-04-01', 'Database Questionnaire', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 15, 85),
-(3, 3, 0, 'DATA301', '2019-03-01', '2019-04-01', 'Database Questionnaire', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 15, 89),
-(3, 4, 0, 'DATA301', '2019-03-01', '2019-04-01', 'Database Questionnaire', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 15, 82),
-(4, 1, 0, 'DATA301', '2019-04-15', '2019-04-29', 'Database Joining and Transactions', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 35, -1),
-(4, 2, 0, 'DATA301', '2019-04-15', '2019-04-29', 'Database Joining and Transactions', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 35, -1),
-(4, 2, 1, 'DATA301', '2019-04-15', '2019-04-29', 'Database Joining and Transactions', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 35, -1),
-(4, 3, 0, 'DATA301', '2019-04-15', '2019-04-29', 'Database Joining and Transactions', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 35, -1),
-(4, 4, 0, 'DATA301', '2019-04-15', '2019-04-29', 'Database Joining and Transactions', 'Morbi molestie pretium purus at finibus. Fusce ultricies neque at feugiat semper. Morbi tincidunt nisi nec libero rhoncus venenatis.', 35, -1);
+INSERT INTO `assignments_table` (`id`, `subject`, `information`, `release_date`, `due_date`, `grade_weight`) VALUES
+(1, 'Front End', 'Nullam quis vulputate urna. Sed pellentesque, arcu id faucibus accumsan, ex tortor dictum arcu, eu molestie magna odio id nibh.', '2019-01-01', '2019-02-01', 20),
+(2, 'Back End', 'Quisque at quam eu ligula rhoncus condimentum bibendum ac libero. Suspendisse in elit euismod, vestibulum turpis eu, pulvinar magna. In in hendrerit risus.', '2019-02-02', '2019-02-28', 30),
+(3, 'Back End', 'Duis fringilla efficitur purus, eu fermentum arcu facilisis et. Nulla quis dictum quam, nec dapibus ipsum. Nullam ex erat, pulvinar ut dolor sit amet, semper rhoncus dolor. ', '2019-03-01', '2019-04-18', 35),
+(4, 'Front End', 'Ivamus posuere sem quis leo suscipit, id volutpat metus tristique. Nulla dictum nunc magna, a consequat eros efficitur at.', '2019-04-15', '2019-04-28', 15);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `students`
+-- Table structure for table `read_announcements`
 --
 
-CREATE TABLE `students` (
-  `id` int(11) NOT NULL,
-  `username` varchar(128) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `name` varchar(64) NOT NULL
+CREATE TABLE `read_announcements` (
+  `announcement_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `students`
+-- Table structure for table `submitted_assignments`
 --
 
-INSERT INTO `students` (`id`, `username`, `password`, `name`) VALUES
-(1, 'student1', '12345678', 'Dylan Doe'),
-(2, 'student2', '12345678', 'James Jackson'),
-(3, 'student3', '12345678', 'Sarah Smith'),
-(4, 'student4', '12345678', 'Miranda Miller');
+CREATE TABLE `submitted_assignments` (
+  `assignment_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `submission` longtext NOT NULL,
+  `grade` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -138,28 +129,59 @@ INSERT INTO `teachers` (`id`, `username`, `password`, `name`) VALUES
 (3, 'teacher3', '12345678', 'Carol Carter'),
 (4, 'teacher4', '12345678', 'Diana Doughty');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `role` varchar(1) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role`, `username`, `password`, `name`) VALUES
+(1, 't', 'teacher1', '12345678', 'Dylan Doe'),
+(2, 't', 'teacher2', '12345678', 'James Jackson'),
+(3, 's', 'student1', '12345678', 'Alan Allen'),
+(4, 's', 'student2', '12345678', 'Bryan Bell'),
+(5, 's', 'student3', '12345678', 'Sarah Smith'),
+(6, 's', 'student4', '12345678', 'Miranda Miller');
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `announcements`
+-- Indexes for table `announcements_table`
 --
-ALTER TABLE `announcements`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `teacherid` (`teacherID`);
-
---
--- Indexes for table `assignments`
---
-ALTER TABLE `assignments`
-  ADD PRIMARY KEY (`id`,`userid`,`teachercopy`);
-
---
--- Indexes for table `students`
---
-ALTER TABLE `students`
+ALTER TABLE `announcements_table`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `assignments_table`
+--
+ALTER TABLE `assignments_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `read_announcements`
+--
+ALTER TABLE `read_announcements`
+  ADD PRIMARY KEY (`announcement_id`,`student_id`);
+
+--
+-- Indexes for table `submitted_assignments`
+--
+ALTER TABLE `submitted_assignments`
+  ADD PRIMARY KEY (`assignment_id`,`student_id`);
 
 --
 -- Indexes for table `teachers`
@@ -168,19 +190,25 @@ ALTER TABLE `teachers`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `announcements`
+-- AUTO_INCREMENT for table `announcements_table`
 --
-ALTER TABLE `announcements`
+ALTER TABLE `announcements_table`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `students`
+-- AUTO_INCREMENT for table `assignments_table`
 --
-ALTER TABLE `students`
+ALTER TABLE `assignments_table`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
@@ -190,14 +218,26 @@ ALTER TABLE `teachers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `announcements`
+-- Constraints for table `read_announcements`
 --
-ALTER TABLE `announcements`
-  ADD CONSTRAINT `teacherid` FOREIGN KEY (`teacherID`) REFERENCES `teachers` (`id`);
+ALTER TABLE `read_announcements`
+  ADD CONSTRAINT `announcement_id` FOREIGN KEY (`announcement_id`) REFERENCES `announcements_table` (`id`);
+
+--
+-- Constraints for table `submitted_assignments`
+--
+ALTER TABLE `submitted_assignments`
+  ADD CONSTRAINT `assignment_id` FOREIGN KEY (`assignment_id`) REFERENCES `assignments_table` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
