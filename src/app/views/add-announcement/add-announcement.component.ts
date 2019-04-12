b74460e1807f68c1bee9536d6237baa92c497276
Meir Donath <donathme@gmail.com>
2019-04-12 15:46:07 -0400

    Added documentation and comments to add-announcements and add-assignments.

--- src/app/views/add-announcement/add-announcement.component.ts
+++ src/app/views/add-announcement/add-announcement.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+/*
+* Authors: Dmitry Bashmakov, Mathias Donath, Josh Fagen, Lidiya Sokolovskya
+* Date Created: April 10, 2019
+* Last Modified: April 12, 2019
+* Main Purpose: TypeScript component for filling out form to add an announcement by a teacher and submitting it to the announcement board.
+*/
+
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-add-announcement',
-  templateUrl: './add-announcement.component.html',
-  styleUrls: ['./add-announcement.component.css']
+    selector: 'app-add-announcement',
+    templateUrl: './add-announcement.component.html',
+    styleUrls: ['./add-announcement.component.css']
 })
 export class AddAnnouncementComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
