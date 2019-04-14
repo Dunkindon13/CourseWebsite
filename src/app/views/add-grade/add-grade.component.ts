498548f36c126249f965eaedb2db71063407ff32
Meir Donath <donathme@gmail.com>
2019-04-14 14:44:33 -0400

    Added documentation and comments to add-announcements, add-assignments, announcements and assignments. Added router link buttons (HTML) and styling for teacher's homepage and student's home page.

--- src/app/views/add-grade/add-grade.component.ts
+++ src/app/views/add-grade/add-grade.component.ts
@@ -1,41 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+/*
+* Authors: Dmitry Bashmakov, Mathias Donath, Josh Fagen, Lidiya Sokolovskya
+* Date Created: April 9, 2019
+* Last Modified: April 11, 2019
+* Main Purpose: TypeScript component to add a grade to an assignment by a teacher and submitting it.
+*/
+import {Component, OnInit} from '@angular/core';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
+    name: string;
+    position: number;
 
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen'},
-  {position: 2, name: 'Helium'},
+    {position: 1, name: 'Hydrogen'},
+    {position: 2, name: 'Helium'},
 
 ];
 
 
-
-
-
 @Component({
-  selector: 'app-add-grade',
-  templateUrl: './add-grade.component.html',
-  styleUrls: ['./add-grade.component.css']
+    selector: 'app-add-grade',
+    templateUrl: './add-grade.component.html',
+    styleUrls: ['./add-grade.component.css']
 })
 
 
+export class AddGradeComponent {
+    displayedColumns: string[] = ['Student ID', 'Student Name'];
+    columnsToDisplay: string[] = this.displayedColumns.slice();
+    data: PeriodicElement[] = ELEMENT_DATA;
+
+    constructor() {
+    }
 
 
+    addGrade() {
 
-export class AddGradeComponent  {
-  displayedColumns: string[] = ['Student ID', 'Student Name'];
-  columnsToDisplay: string[] = this.displayedColumns.slice();
-  data: PeriodicElement[] = ELEMENT_DATA;
-
-  constructor() { }
-
-
-  addGrade() {
-
-}
+    }
 
 }
--- src/app/views/announcements/announcements.component.ts
+++ src/app/views/announcements/announcements.component.ts
@@ -1,33 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+/*
+* Authors: Dmitry Bashmakov, Mathias Donath, Josh Fagen, Lidiya Sokolovskya
+* Date Created: April 8, 2019
+* Last Modified: April 10, 2019
+* Main Purpose: TypeScript component to show all announcements currently on the announcement board.
+*/
+import {Component, OnInit} from '@angular/core';
 import {AnnouncementsService} from '../../models/announcements.service';
 import {Announcement} from '../../models/announcement';
 
 @Component({
-  selector: 'app-announcements',
-  templateUrl: './announcements.component.html',
-  styleUrls: ['./announcements.component.css']
+    selector: 'app-announcements',
+    templateUrl: './announcements.component.html',
+    styleUrls: ['./announcements.component.css']
 })
 export class AnnouncementsComponent implements OnInit {
-  announcements: Announcement[];
-  error = '';
-  success = '';
-  teacherIsLoggedIn = true;
-  studentIsLoggedIn = false;
-  constructor(private announcementsServ: AnnouncementsService) { }
+    announcements: Announcement[];
+    error = '';
+    success = '';
+    teacherIsLoggedIn = true;
+    studentIsLoggedIn = false;
 
-  ngOnInit() {
-    this.getAnnouncements();
-  }
-  getAnnouncements(): void {
-    this.announcementsServ.getAll().subscribe(
-      (res: Announcement[]) => {
-        console.log('This Works');
-        this.announcements = res;
-      },
-        (err) => {
-          this.error = err;
-        }
-    );
-  }
+    constructor(private announcementsServ: AnnouncementsService) {
+    }
+
+    ngOnInit() {
+        this.getAnnouncements();
+    }
+
+    getAnnouncements(): void {
+        this.announcementsServ.getAll().subscribe(
+            (res: Announcement[]) => {
+                console.log('This Works');
+                this.announcements = res;
+            },
+            (err) => {
+                this.error = err;
+            }
+        );
+    }
 }
 
